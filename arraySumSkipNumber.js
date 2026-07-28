1d1423ce11f473cec7f80de6bc5bcfdc700125cd
Hridoy-Ahmed-Rafi20 <hridoyahmedrafi20@gmail.com>
2026-07-28 18:31:05 +0600

    array sum skip number bug fixed

--- arraySumSkipNumber.js
+++ arraySumSkipNumber.js
@@ -4,3 +4,23 @@ Input : [1, 2, 3, 4]
 Expected Output : 10 
  */
 
+// Buggy Code:
+// function sumArray(numbers) {
+//   let total = 0;
+//   for (let i = 1; i < numbers.length; i++) { // find the bug
+//     total = total + numbers[i];
+//   }
+//   return total;
+// }
+// console.log(sumArray([1, 2, 3, 4])); // Expected: 10
+
+// Fixed Bug:
+function sumArray(numbers) {
+  let total = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    // here was initial value 1, but initial value should 0 if wanna sum of all numbers
+    total = total + numbers[i];
+  }
+  return total;
+}
+console.log(sumArray([1, 2, 3, 4])); // Expected: 10

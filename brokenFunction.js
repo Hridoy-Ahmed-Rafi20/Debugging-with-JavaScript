0e1ca079c17335d0202ac10b0e7bd2420019ec5a
Hridoy-Ahmed-Rafi20 <hridoyahmedrafi20@gmail.com>
2026-07-28 17:49:46 +0600

    broken function bug fixed

--- brokenFunction.js
+++ brokenFunction.js
@@ -8,14 +8,14 @@ Expected Output : 7
 
 /* Bug */
 // Bug: find why the result comes out negative
-function subtract(a, b) {
-  return b - a; // find the bug
-}
-console.log(subtract(10, 3)); // Expected: 7 (currently returns -7)
+// function subtract(a, b) {
+//   return b - a; // find the bug
+// }
+// console.log(subtract(10, 3)); // Expected: 7 (currently returns -7)
 
 // Fixed Bug
 // Bug: find why the result comes out negative
 function subtract(a, b) {
-  return b - a; // find the bug
+  return a - b; // there was b-a, that should a-b
 }
 console.log(subtract(10, 3)); // Expected: 7 (currently returns -7)

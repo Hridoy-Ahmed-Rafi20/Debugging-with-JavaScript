fb4adaa151898910e7db83ff29d80d41ba085a54
Hridoy-Ahmed-Rafi20 <hridoyahmedrafi20@gmail.com>
2026-07-28 18:13:50 +0600

    applyed full proccess of debugging

---
+++ applyFullDebuggingProcess.js
@@ -0,0 +1,38 @@
+/* Task 14-8: Apply the Full Debugging Process 
+Problem Statement: 
+This function should return how many vowels are in a string. Go through the full checklist: does it 
+run? does it crash? is the answer wrong? isolate it with console.log, then fix it. 
+Input : "orange"
+Expected Output : 3
+ */
+
+/* Bug */
+// Apply the full debugging process to find this bug
+// function countVowels(str) {
+//   let vowels = "aeiou";
+//   let count = 0;
+//   for (let i = 0; i <= str.length; i++) {
+//     // find the bug
+//     if (vowels.includes(str[i])) {
+//       count = count + 1;
+//     }
+//   }
+//   return count;
+// }
+// console.log(countVowels("orange")); // Expected: 3
+
+// Fixed Bug
+// Apply the full debugging process to find this bug
+function countVowels(str) {
+  // here is no any bugs, code running correctly, code not crashing, giving correct answer
+  let vowels = "aeiou";
+  let count = 0;
+  for (let i = 0; i <= str.length; i++) {
+    // find the bug
+    if (vowels.includes(str[i])) {
+      count = count + 1;
+    }
+  }
+  return count;
+}
+console.log(countVowels("orange")); // Expected: 3

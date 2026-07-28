c4bf0d5b241208ea2aea9bf898fca0ac5dce5c39
Hridoy-Ahmed-Rafi20 <hridoyahmedrafi20@gmail.com>
2026-07-28 16:58:36 +0600

    but-2 fixed

--- spotTheError.js
+++ spotTheError.js
@@ -2,7 +2,7 @@
 Below are three small broken snippets. For each, identify the error type (Syntax / Runtime / Logic), 
 then fix it. */
 
-// Snippet A
+
 /* Bug-1 */
 // function sayHi(name) {
 // console.log("Hi, " + name  // find the bug
@@ -12,8 +12,23 @@ then fix it. */
 // Input : sayHi("Mira")
 // Expected Output  : Hi, Mira
 
-// fixed Bug
+// fixed Bug-1
 function sayHi(name) {
   console.log("Hi, " + name); // find the bug
 }
 sayHi("Mira");
+
+
+/* Bug-2 */
+// let word = "hello";
+// console.log(word.toUppercase()); // find the bug
+
+// Input :word.toUppercase()
+// Expected Output : HELLO
+
+// fixed Bug-2
+let word = "hello";
+console.log(word.toUpperCase()); // find the bug
+
+// Input :word.toUppercase()
+// Expected Output : HELLO
